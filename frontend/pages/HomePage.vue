187e78a68baf89ed0233b0ad7402af9cba422d4d
aryabanda189a <aryabanda189a@gmail.com>
2026-03-10 09:35:11 +0530

    Update HomePage.vue

--- frontend/pages/HomePage.vue
+++ frontend/pages/HomePage.vue
@@ -83,7 +83,7 @@ export default {
       this.category = null;
 
       try {
-        const res = await fetch("http://127.0.0.1:5000/login", {
+        const res = await fetch("https://hms-eb5j.onrender.com/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
