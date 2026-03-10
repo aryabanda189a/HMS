d7cd2e47645916913deb1813bef56f8720e09821
aryabanda189a <aryabanda189a@gmail.com>
2026-03-10 09:50:03 +0530

    Update PatientDoctorsPage.vue

--- frontend/pages/PatientDoctorsPage.vue
+++ frontend/pages/PatientDoctorsPage.vue
@@ -154,7 +154,7 @@ export default {
       }
 
       try {
-        const response = await fetch(`http://127.0.0.1:5000/customer/search`, {
+        const response = await fetch(`https://hms-eb5j.onrender.com/customer/search`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
